fe4bed546d7308e9bf4495601bddb6eedc187356
Abdeltif Bouziane <contact@eyetags.net>
2019-05-21 04:52:25 +0000

    Punsh game heroku deploy

---
+++ build/precache-manifest.68ab75c7c9d0b7cfb8316e4527585c61.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88b730e5cb2b752266c5cb1efc681f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6994790dd2c56ebee08a",
+    "url": "/static/css/main.5ea2c55f.chunk.css"
+  },
+  {
+    "revision": "4af5d994d4ad75144c6d",
+    "url": "/static/js/2.ee670bdf.chunk.js"
+  },
+  {
+    "revision": "6994790dd2c56ebee08a",
+    "url": "/static/js/main.e1e1b4ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8601bea353c977b2ca9a3334916f494a",
+    "url": "/static/media/ar-hand.8601bea3.png"
+  },
+  {
+    "revision": "9bf3f0821aee3c5d4b1a5006e657a517",
+    "url": "/static/media/ar-head.9bf3f082.png"
+  },
+  {
+    "revision": "58704f69ca36b3c77f6e256d9c3a8245",
+    "url": "/static/media/policeman-foot.58704f69.png"
+  },
+  {
+    "revision": "0801583fce981d7a37188a8d653d5dc0",
+    "url": "/static/media/policeman.0801583f.png"
+  },
+  {
+    "revision": "c00cc1ae1926c2e2da737f994dc62e78",
+    "url": "/static/media/punch.c00cc1ae.png"
+  },
+  {
+    "revision": "226d07d9ab43f32d257d52261b02c7c3",
+    "url": "/static/media/smack-png-13.226d07d9.png"
+  }
+]);
